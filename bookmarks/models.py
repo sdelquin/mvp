c5a9726d6d2a4c3bdf6d303ee5c4e543686c84f9
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-12-19 19:00:03 +0000

    Add field to track important bookmarks

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -5,6 +5,7 @@ class Bookmark(models.Model):
     name = models.CharField(max_length=256)
     url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
+    important = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
